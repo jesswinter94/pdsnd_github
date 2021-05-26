88fcdf3d4877341a9fe7f050b04c3d18c941134f
JessWinter <jwinter@kx.com>
2021-05-25 22:19:51 -0400

    Moving view_data function to beginning of script so Users can see the data before stats run

--- bikeshare.py
+++ bikeshare.py
@@ -253,12 +253,11 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
+	view_data(df)
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        view_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
